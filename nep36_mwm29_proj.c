558e8ce426bfff630d1357f9cb7f7739de00a2fd
nperryman1995 <44374031+nperryman1995@users.noreply.github.com>
2018-11-02 15:28:22 -0500

    Add files via upload

--- nep36_mwm29_proj.c
+++ nep36_mwm29_proj.c
@@ -9,35 +9,26 @@
 
 #include <stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
 
-/*Function declarations*/
-void initRegs(struct int_Reg *iR, struct float_Reg *fR);
-void writeIntReg(struct int_Reg *iR, char *reg, uint32_t val);
-void writeFloatReg(struct float_Reg *fR, char *reg, float val);
-uint32_t getIntReg(struct int_Reg *iR, char *reg);
-float getFloatReg(struct float_Reg *fR, char *reg);
-int regLookup(char *reg);
-void assignInstr(struct instruction *instr);
-void printInstr(struct instruction *instr);
-void showIntReg(struct int_Reg *iR);
-void showFPReg(struct float_Reg *fR);
+#define INPUT_SIZE 1000
 
 /*Instruction struct to take in all of the instruction types*/
 struct instruction {
 	unsigned char type;
-	char* Ra;
-	char* Fa;
-	char* Rs;
-	char* Fs;
-	char* Rt;
-	char* Ft;
+	char Ra[4];
+	char Fa[4];
+	char Rs[4];
+	char Fs[4];
+	char Rt[4];
+	char Ft[4];
 	uint32_t offset; //For both offsets and immediates
 	//uint32_t PC_Addr;
 };
 
 /*Encoding for instruction struct type*/
 enum opcode_type {
-	ti_Ld, //Load a single precision floating point value
+	ti_Ld = 0, //Load a single precision floating point value
 	ti_Sd, //Store a single precision floating point value to memory
 	ti_Beq, //Branch if equal
 	ti_Bne, //Branch if not equal
@@ -60,6 +51,19 @@ struct float_Reg {
 	float F_num[32];
 };
 
+/*Function declarations*/
+void initRegs(struct int_Reg *iR, struct float_Reg *fR);
+void writeIntReg(struct int_Reg *iR, char *reg, uint32_t val);
+void writeFloatReg(struct float_Reg *fR, char *reg, float val);
+uint32_t getIntReg(struct int_Reg *iR, char *reg);
+float getFloatReg(struct float_Reg *fR, char *reg);
+int regLookup(char *reg);
+void assignInstr(struct instruction *instr, char *temp);
+void printInstr(struct instruction instr);
+void showIntReg(struct int_Reg *iR);
+void showFPReg(struct float_Reg *fR);
+void showMemory(double memData[256]);
+
 int main(int argc, char **argv)
 {
 	unsigned int cycle_number = 0;
@@ -67,7 +71,8 @@ int main(int argc, char **argv)
 	struct float_Reg fR;
 	initRegs(&iR, &fR); //initialize integer and floating point regsiters
 	
-	struct instruction *entry; //instruction struct for taking in new instructions
+	//static struct instruction *entry; //instruction struct for taking in new instructions
+	struct instruction entry[INPUT_SIZE];
 	struct instruction IS, EX, MEM, WB, COM; //instruction structs for pipeline stages
 	
 	char *fileName; //text file name
@@ -99,7 +104,7 @@ int main(int argc, char **argv)
 		fprintf(stdout, "\ntrace file %s not opened.\n\n", fileName);
 		exit(0);
 	}
-	
+
 	//read text file inputs
 	fscanf(trace_fd, "%d %d %d %d", &intAdd_rs, &intAdd_EX_Cycles, &intAdd_MEM_cycles, &intAdd_FUs);
 	fscanf(trace_fd, "%d %d %d %d", &FPAdd_rs, &FPAdd_EX_Cycles, &FPAdd_MEM_cycles, &FPAdd_FUs);
@@ -109,63 +114,82 @@ int main(int argc, char **argv)
 	fscanf(trace_fd, "%d %d %d", &IntregInits, &FPregInits, &MemInits);
 	fscanf(trace_fd, "%d", &numInstr);
 	
-	char *regtemp;
-	int regtempVal;
+	//Test if inputs were read in correctly
+	/*printf("%d %d %d %d\n", intAdd_rs, intAdd_EX_Cycles, intAdd_MEM_cycles, intAdd_FUs);
+	printf("%d %d %d %d\n", FPAdd_rs, FPAdd_EX_Cycles, FPAdd_MEM_cycles, FPAdd_FUs);
+	printf("%d %d %d %d\n", FPMult_rs, FPMult_EX_Cycles, FPMult_MEM_cycles, FPMult_FUs);
+	printf("%d %d %d %d\n", ld_sd_rs, ld_sd_EX_Cycles, ld_sd_MEM_cycles, ld_sd_FUs);
+	printf("%d %d\n", ROB_Entries, CDB_Buffer_Entries);
+	printf("%d %d %d\n", IntregInits, FPregInits, MemInits);
+	printf("%d\n", numInstr);*/
+	
+	char regtemp[50];
+	uint32_t regtempVal;
 	float regtempValFP;
 	for(i = 0; i < IntregInits; i++) {
 		fscanf(trace_fd, "%s %d", regtemp, &regtempVal);
-		writeIntReg(iR, regtemp, regtempVal);
+		writeIntReg(&iR, regtemp, regtempVal);
 	}
-	
+    
 	for(i = 0; i < FPregInits; i++) {
-		fscanf(trace_fd, "%s, %f", regtempValFP);
-		writeFloatReg(fR, regtemp, regtempValFP);
+		fscanf(trace_fd, "%s %f", regtemp, &regtempValFP);
+		writeFloatReg(&fR, regtemp, regtempValFP);
 	}
-	
+
 	for(i = 0; i < MemInits; i++) {
-		fscanf(trace_fd, "%s %d %lf", regtemp, &regtempVal, &regtempValFP);
+		fscanf(trace_fd, "%s %d %f", regtemp, &regtempVal, &regtempValFP);
 		memData[regtempVal] = regtempValFP;
 	}
 	
-	entry = malloc(sizeof(struct instruction) * numInstr);
+	//Test if inputs were read in correctly
+	/*showIntReg(&iR);
+	showFPReg(&fR);
+	showMemory(memData);*/
+	
+	//entry = (struct instruction *)malloc(sizeof(struct instruction)*numInstr);
 	for(i = 0; i < numInstr; i++) {
 		fscanf(trace_fd, "%s", regtemp);
-		assignInstr(entry[i], regtemp);
+		assignInstr(&entry[i], regtemp);
 		switch(entry[i].type) {
 			case ti_Ld:
-				fscanf(trace_fd, "%s, %d(%s)", entry[i].Fa, entry[i].offset, entry[i].Ra);
+				fscanf(trace_fd, "%*c %3[^,] %*c %d %*c %3[^)] %*c", entry[i].Fa, &entry[i].offset, entry[i].Ra);
 				break;
 			case ti_Sd:
-				fscanf(trace_fd, "%s, %d(%s)", entry[i].Fa, entry[i].offset, entry[i].Ra);
+				fscanf(trace_fd, "%*c %3[^,] %*c %d %*c %3[^)] %*c", entry[i].Fa, &entry[i].offset, entry[i].Ra);
 				break;		
 			case ti_Beq:
-				fscanf(trace_fd, "%s, %s, %d", entry[i].Rs, entry[i].Rt, entry[i].offset);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %d", entry[i].Rs, entry[i].Rt, &entry[i].offset);
 				break;		
 			case ti_Bne:
-				fscanf(trace_fd, "%s, %s, %d", entry[i].Rs, entry[i].Rt, entry[i].offset);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %d", entry[i].Rs, entry[i].Rt, &entry[i].offset);
 				break;		
 			case ti_Add:
-				fscanf(trace_fd, "%s, %s, %s", entry[i].Rd, entry[i].Rs, entry[i].Rt);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %s", entry[i].Ra, entry[i].Rs, entry[i].Rt);
 				break;		
 			case ti_Addd:
-				fscanf(trace_fd, "%s, %s, %s", entry[i].Fd, entry[i].Fs, entry[i].Ft);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %s", entry[i].Fa, entry[i].Fs, entry[i].Ft);
 				break;		
 			case ti_Addi:
-				fscanf(trace_fd, "%s, %s, %d", entry[i].Rt, entry[i].Rs, entry[i].offset);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %d", entry[i].Rt, entry[i].Rs, &entry[i].offset);
 				break;		
 			case ti_Sub:
-				fscanf(trace_fd, "%s, %s, %s", entry[i].Rd, entry[i].Rs, entry[i].Rt);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %s", entry[i].Ra, entry[i].Rs, entry[i].Rt);
 				break;		
 			case ti_Subd:
-				fscanf(trace_fd, "%s, %s, %s", entry[i].Fd, entry[i].Fs, entry[i].Ft);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %s", entry[i].Fa, entry[i].Fs, entry[i].Ft);
 				break;		
 			case ti_Multd:
-				fscanf(trace_fd, "%s, %s, %s", entry[i].Fd, entry[i].Fs, entry[i].Ft);
+				fscanf(trace_fd, "%*c %3[^,] %*c %3[^,] %*c %s", entry[i].Fa, entry[i].Fs, entry[i].Ft);
 				break;		
 		}
+		//printInstr(entry[i]);
 	}
   
-	free(entry);
+	//Test Get functions
+	/*printf("R2 = %d\n", getIntReg(&iR, "R2"));
+	printf("F2 = %.2f\n", getFloatReg(&fR, "F2"));*/
+  
+	//free(entry);
 	fclose(trace_fd);
 	return 1;
   
@@ -196,7 +220,7 @@ void writeIntReg(struct int_Reg *iR, char *reg, uint32_t val) {
 /*Write to the specified floating point register*/
 void writeFloatReg(struct float_Reg *fR, char *reg, float val) {
 	int rN = regLookup(reg);
-	fR->f_num[rN] = val;
+	fR->F_num[rN] = val;
 }
 
 /*Get the specified integer register*/
@@ -208,7 +232,7 @@ uint32_t getIntReg (struct int_Reg *iR, char *reg) {
 /*Get the specified floating point register*/
 float getFloatReg(struct float_Reg *fR, char *reg) {
 	int rN = regLookup(reg);
-	return fR->R_num[rN];
+	return fR->F_num[rN];
 }
 
 /*Given the register string, find the register number*/
@@ -227,23 +251,23 @@ int regLookup(char *reg) {
 
 /*Assign the type field of the instruction*/
 void assignInstr(struct instruction *instr, char *temp) {
-	if(!strcmp(???, "Ld")){
+	if(!strcmp(temp, "Ld")){
 		instr->type = ti_Ld;
-	}else if(!strcmp(???,"Sd")) {
+	}else if(!strcmp(temp,"Sd")) {
 		instr->type = ti_Sd;
-	}else if(!strcmp(???,"Beq")) {
+	}else if(!strcmp(temp,"Beq")) {
 		instr->type = ti_Beq;
-	}else if(!strcmp(???,"Bne")) {
+	}else if(!strcmp(temp,"Bne")) {
 		instr->type = ti_Bne;
-	}else if(!strcmp(???,"Add")) {
+	}else if(!strcmp(temp,"Add")) {
 		instr->type = ti_Add;
-	}else if(!strcmp(???,"Add.d")) {
+	}else if(!strcmp(temp,"Add.d")) {
 		instr->type = ti_Addd;
-	}else if(!strcmp(???,"Addi")) {
+	}else if(!strcmp(temp,"Addi")) {
 		instr->type = ti_Addi;
-	}else if(!strcmp(???,"Sub")) {
+	}else if(!strcmp(temp,"Sub")) {
 		instr->type = ti_Sub;
-	}else if(!strcmp(???,"Sub.d")) {
+	}else if(!strcmp(temp,"Sub.d")) {
 		instr->type = ti_Subd;
 	}else {
 		instr->type = ti_Multd;
@@ -251,46 +275,49 @@ void assignInstr(struct instruction *instr, char *temp) {
 }
 
 /*print the instruction*/
-void printInstr(struct instruction *instr) {
-	switch(instr->type) {
+void printInstr(struct instruction instr) {
+	switch(instr.type) {
 		case ti_Ld:
-			printf("Ld %s, %d(%s)",instr->Fa, instr->offset, instr->Ra);
+			printf("Ld %s, %d(%s)",instr.Fa, instr.offset, instr.Ra);
 			break;
 		case ti_Sd:
-			printf("Sd %s, %d(%s)",instr->Fa, instr->offset, instr->Ra);
+			printf("Sd %s, %d(%s)",instr.Fa, instr.offset, instr.Ra);
 			break;		
 		case ti_Beq:
-			printf("Beq %s, %s, %d",instr->Rs, instr->Rt, instr->offset);
+			printf("Beq %s, %s, %d",instr.Rs, instr.Rt, instr.offset);
 			break;		
 		case ti_Bne:
-			printf("Bne %s, %s, %d",instr->Rs, instr->Rt, instr->offset);
+			printf("Bne %s, %s, %d",instr.Rs, instr.Rt, instr.offset);
 			break;		
 		case ti_Add:
-			printf("Add %s, %s, %s", instr->Rd, instr->Rs, instr->Rt);
+			printf("Add %s, %s, %s", instr.Ra, instr.Rs, instr.Rt);
 			break;		
 		case ti_Addd:
-			printf("Add.d %s, %s, %s", instr->Fd, instr->Fs, instr->Ft);
+			printf("Add.d %s, %s, %s", instr.Fa, instr.Fs, instr.Ft);
 			break;		
 		case ti_Addi:
-			printf("Addi %s, %s, %d", instr->Rd, instr->Rs, instr->offset);
+			printf("Addi %s, %s, %d", instr.Rt, instr.Rs, instr.offset);
 			break;		
 		case ti_Sub:
-			printf("Sub %s, %s, %s", instr->Rd, instr->Rs, instr->Rt);
+			printf("Sub %s, %s, %s", instr.Ra, instr.Rs, instr.Rt);
 			break;		
 		case ti_Subd:
-			printf("Sub.d %s, %s, %s", instr->Fd, instr->Fs, instr->Ft);
+			printf("Sub.d %s, %s, %s", instr.Fa, instr.Fs, instr.Ft);
 			break;		
 		case ti_Multd:
-			printf("Mult.d %s, %s, %s", instr->Fd, instr->Fs, instr->Ft);
+			printf("Mult.d %s, %s, %s", instr.Fa, instr.Fs, instr.Ft);
 			break;		
 	}
+	printf("\n");
 }
 
 /*Show contents of integer register files*/
 void showIntReg(struct int_Reg *iR) {
 	int i;
 	for(i = 0; i < 32; i++) {
-		printf("R%d = %d",i, iR->R_Num[i]);
+		if(iR->R_num[i] != 0) {	
+			printf("R%d = %d\n",i, iR->R_num[i]);
+		}
 	}
 }
 
@@ -298,6 +325,17 @@ void showIntReg(struct int_Reg *iR) {
 void showFPReg(struct float_Reg *fR) {
 	int i;
 	for(i = 0; i < 32; i++) {
-		printf("F%d = %d",i, fR->F_Num[i]);
+		if(fR->F_num[i] !=0) {
+			printf("F%d = %.2f\n",i, fR->F_num[i]);
+		}
+	}	
+}
+
+void showMemory(double *memData) {
+	int i;
+	for(i = 0; i < 256; i+=4) {
+		if(memData[i] != 0) {
+			printf("Mem[%d] = %.2lf\n", i, memData[i]);
+		}
 	}	
 }
